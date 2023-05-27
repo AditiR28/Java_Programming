6efebf80a1a7664790a2eea024740c8c6a665824
AditiR28 <rajput.28aditi@gmail.com>
2023-05-27 20:37:23 +0530

    loops learning

---
+++ forr.java
@@ -0,0 +1,9 @@
+class forr{
+    public static void main(String args[]){
+        int i =0;
+        while(i<11){
+            System.out.println(i);
+            i =i+1;
+        }
+    }
+}
---
+++ loops.java
@@ -0,0 +1,9 @@
+class loops {
+    public static void main (String args[]){
+        //counter++ =counter+1
+        for(int i =0;i < 11;i++){
+        System.out.println(i);// output print value in next line
+        //system.out.println(counter + "") //output print in single line
+    }
+}
+}
