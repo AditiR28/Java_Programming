6fe77a1c3f3ddeebe073062677d4f41880afffa7
AditiR28 <rajput.28aditi@gmail.com>
2023-05-28 16:55:12 +0530

    functions learning

---
+++ functions/name.java
@@ -0,0 +1,20 @@
+package Functions;
+
+import java.util.Scanner;
+
+public class name {
+    public static void printMyName(String name){
+        System.out.println(name);
+        
+
+    }
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        String name =sc.next();
+        printMyName(name);
+
+    }
+
+    }
+    
+
---
+++ functions/sum.java
@@ -0,0 +1,22 @@
+package Functions;
+
+import java.util.Scanner;
+
+public class sum {
+    public static int summynum (int a,int b){
+int sum = a+b;
+return sum;
+    }
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a number a");
+        int a = sc.nextInt();
+        System.out.println("Enter a number b");
+        
+        int b = sc.nextInt();
+        int sum =summynum(a, b);
+        System.out.println("Sum of two number =" + sum);
+
+    }
+    
+}
