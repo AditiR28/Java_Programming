52519e75858921db03937815214eae152d841f56
AditiR28 <rajput.28aditi@gmail.com>
2023-07-13 12:53:56 +0530

    testing new stup

--- even.java
+++ even.java
@@ -5,7 +5,7 @@ class even{
         Scanner sc = new Scanner(System.in);
         int n =sc.nextInt();
         for(int i =1;i<=n;i++){
-           if(i%2 == 0)
+           if(i%2 == 0);
                 System.out.println(i);
             
         }
