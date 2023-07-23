0092602232c69dc5f4113a71f5afeb0c9d58709b
AditiR28 <rajput.28aditi@gmail.com>
2023-07-23 16:56:41 +0530

    swap numbers using third variable

--- codelogic/swapthird.java
+++ codelogic/swapthird.java
@@ -1,9 +1,30 @@
 package codelogic;
 
-public class swapthird {
+import java.util.Scanner;
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+public class swapthird {
+	public void thirdvar() {
+		System.out.println("Swapping of two variable using third variable");
+		Scanner ob = new Scanner(System.in);
+		System.out.println("Enter the value of a :");
+		int a = ob.nextInt();
+		System.out.println("Enter the value of b :");
+		int b =ob.nextInt();
+		//implement the code logic
+		 int c;
+		 c= a+b;
+		 b = c-a;
+		 a = c-b;
+		System.out.println("A ="+a);
+		System.out.println("B ="+b);
+		
+		
+	}
+
+      public static void main(String[] args) {
+     swapthird obj1 = new swapthird();
+     obj1.thirdvar();
+	
 
 	}
 
